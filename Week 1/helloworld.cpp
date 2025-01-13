bc2e3688e9dfd350ec388bcc46034a9ac740b001
bryce <bryceriordan2005@gmail.com>
2025-01-13 11:41:56 -0600

    day 1

---
+++ Week 1/helloworld.cpp
@@ -0,0 +1,14 @@
+//
+// Created by bryce on 1/13/2025.
+//
+
+#include "helloworld.h"
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!\n";
+  return 0;
+  }
+
+// easiest program ever
---
+++ Week 1/helloworld.h
@@ -0,0 +1,16 @@
+//
+// Created by bryce on 1/13/2025.
+//
+
+#ifndef HELLOWORLD_H
+#define HELLOWORLD_H
+
+
+
+class helloworld {
+
+};
+
+
+
+#endif //HELLOWORLD_H
