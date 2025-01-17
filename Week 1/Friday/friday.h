f559e3433a9d9b6969caa9c9c6571ee31dee87e8
bryce <bryceriordan2005@gmail.com>
2025-01-17 11:28:50 -0600

    friday changes and additions

---
+++ Week 1/Friday/friday.h
@@ -0,0 +1,16 @@
+//
+// Created by bryce on 1/17/2025.
+//
+
+#ifndef FRIDAY_H
+#define FRIDAY_H
+
+
+
+class fridayOpener {
+
+};
+
+
+
+#endif //FRIDAY_H
---
+++ Week 1/Friday/fridayOpener.cpp
@@ -0,0 +1,18 @@
+//
+// Created by bryce on 1/17/2025.
+//
+
+#include "friday.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+  string studentName = "Bryce";
+
+  int testScores[3] = {90, 85, 88};
+
+  cout << "Student: " << studentName << endl;
+  cout << "Test 1: " << testScores[0] << endl;
+  cout << "Test 2: " << testScores[1] << endl;
+  cout << "Test 3: " << testScores[2] << endl;
+}
