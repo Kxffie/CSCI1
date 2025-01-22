08ea845e44aa206ed28980e74a8dfdabe138a778
bryce <bryceriordan2005@gmail.com>
2025-01-22 11:06:24 -0600

    friday changes and additions

---
+++ Classes/Week 2/Wednesday/numvariables.cpp
@@ -0,0 +1,18 @@
+//
+// Created by bryce on 1/22/2025.
+//
+
+#include "numvariables.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+  int num1 = 23;
+  int num2 = 5;
+  cout << num1 + num2 << endl;
+  cout << num1 - num2 << endl;
+  cout << num1 * num2 << endl;
+  cout << num1 / num2 << endl;
+  cout << num1 % num2 << endl;
+  return 0;
+}
---
+++ Classes/Week 2/Wednesday/numvariables.h
@@ -0,0 +1,16 @@
+//
+// Created by bryce on 1/22/2025.
+//
+
+#ifndef NUMVARIABLES_H
+#define NUMVARIABLES_H
+
+
+
+class numvariables {
+
+};
+
+
+
+#endif //NUMVARIABLES_H
