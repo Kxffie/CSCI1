8b0daacca15afdc24e0ad9983ff96ddc3c3c7916
bryce <bryceriordan2005@gmail.com>
2025-04-03 11:30:42 -0500

    c

---
+++ fun/test.cpp
@@ -0,0 +1,15 @@
+//
+// Created by bryce on 4/3/2025.
+//
+
+#include "test.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// cout << toupper('b');
+	cout << isalpha('g');
+	return 0;
+
+}
---
+++ fun/test.h
@@ -0,0 +1,16 @@
+//
+// Created by bryce on 4/3/2025.
+//
+
+#ifndef TEST_H
+#define TEST_H
+
+
+
+class test {
+
+};
+
+
+
+#endif //TEST_H
